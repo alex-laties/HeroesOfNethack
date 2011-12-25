ec0d961349b907ad89b60ab8599ed03b00d6215a
Alex Laties <alex.laties@gmail.com>
2011-12-25 06:32:30 -0500

    began proper work on printing out state :)

--- client/js/hon.js
+++ client/js/hon.js
@@ -14,6 +14,7 @@ hon.Character = new function (_name, _hp, _mp, _agi, _agiG, _int, _intG, _str, _
   this.a2 = _ab2;
   this.a3 = _ab3;
   this.a4 = _ab4;
+  this.items = [];
   this.buffs = [];
   this.debuffs = [];
   this.currentHP = 0;
@@ -22,4 +23,15 @@ hon.Character = new function (_name, _hp, _mp, _agi, _agiG, _int, _intG, _str, _
 
 hon.Init = function () {
   
+}
+
+hon.PrintState = function () {
+  $("name")[0].textContent("Name: " + HoN.State.player.name);
+  $("hp")[0].textContent("HP: " + HoN.State.player.currentHP + "/" + HoN.State.player.hp);
+  $("mp")[0].textContent("MP: " + HoN.State.player.currentMP + "/" + HoN.State.player.mp);
+  $("agi")[0].textContent("Agility: " + HoN.State.player.agility);
+  $("int")[0].textContent("Intelligence: " + HoN.State.player.intel);
+  $("str")[0].textContent("Strength: " + HoN.State.player.strength);
+  
+  //TODO print items and abilities
 }
