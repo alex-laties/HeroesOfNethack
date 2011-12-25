5ee37c0d2eb86833343569bb2ff833e70729232d
Alex Laties <alex.laties@gmail.com>
2011-12-25 03:33:11 -0500

    more debug code

--- client/js/world_worker.js
+++ client/js/world_worker.js
@@ -4,10 +4,16 @@ function onmessage(event) {
   xhr = new XMLHttpRequest();
   xhr.open('GET', "http://www.aperaturetesting.com:8080/ajax/snapshot");
   xhr.onreadystatechange = function () {
-    if (xhr.readyState ==4)
+    if (xhr.readyState != 4)
     {
-      postMessage({world: xhr.responseText, worldID: 1});
+      if (xhr.status != 200)
+      {
+        alert("oh god i'm on fire!")
+      }
+      return
     }
+    
+    postMessage({world: xhr.responseText, worldID: 1});
   };
   xhr.send();
 }
