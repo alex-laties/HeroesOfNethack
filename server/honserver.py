ea97b44bb95c934cd7cbfa3a2e33c370b8429020
Raymond W. Ko <raymond.w.ko@gmail.com>
2011-12-24 23:57:55 -0500

    testing sessions

--- server/honserver.py
+++ server/honserver.py
@@ -3,17 +3,25 @@ import web
 
 urls = (
     "/", "index",
-    "(/css/.+)", "static",
-    "(/js/.+)", "static",
-    "/ajax?(.+)", "ajax"
+    "(/css/.+)", "staticfiles",
+    "(/js/.+)", "staticfiles",
+    "/ajax/?(.+)", "ajax"
+    "/count", "count"
+    "/reset", "reset"
 )
+
+app = web.application(urls, globals())
+default_session_vars = {}
+default_session_vars[count] = 0
+session = web.session.Session(app, web.session.DiskStore("sessions"),
+                              initializers = default_session_vars)
 
 class index:
     def GET(self):
         f = open("../client/index.html", "r")
         return f.read()
 
-class static:
+class staticfiles:
     def GET(self, name):
         f = open("../client/{0}".format(name), "r")
         return f.read()
@@ -23,7 +31,15 @@ class ajax:
         print command
         return "not yet"
 
-app = web.application(urls, globals())
+class count:
+    def GET(self, command):
+        session.count += 1
+        return str(session.count)
+
+class reset:
+    def GET(self, command):
+        session.kill()
+        return "reset!"
 
 if __name__ == "__main__":
     app.run()
