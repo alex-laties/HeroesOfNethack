e4b22761f05a21ea47b2b1c500edb22580604b5f
Raymond W. Ko <raymond.w.ko@gmail.com>
2011-12-25 01:39:11 -0500

    case problem

--- server/game/world.py
+++ server/game/world.py
@@ -4,15 +4,15 @@ class World:
     size = 16
     def __init__(self):
         self.cells = []
-        for x in xrange(world.size):
+        for x in xrange(World.size):
             self.cells.append([])
-            for y in xrange(world.size):
+            for y in xrange(World.size):
                 self.cells[x].append(cell.Cell())
 
     def __str__(self):
         board = ""
-        for x in xrange(world.size):
-            for y in xrange(world.size):
+        for x in xrange(World.size):
+            for y in xrange(World.size):
                 board += str(self.cells[x][y])
             board += "\n"
 
