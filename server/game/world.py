eb5fa6e49b12acf4c4857dfae9d7135c1c6bf043
Raymond W. Ko <raymond.w.ko@gmail.com>
2011-12-25 03:01:04 -0500

    can create players

--- server/game/world.py
+++ server/game/world.py
@@ -1,16 +1,22 @@
 import cell
 import json
+import player
+import random
 
 class World:
     # hardcoded constants
     size = 16
 
     def __init__(self):
+        # init all the cells of the world
         self.cells = []
         for x in xrange(World.size):
             self.cells.append([])
             for y in xrange(World.size):
                 self.cells[x].append(cell.Cell())
+
+        # this dict will store all the players
+        self.players = {}
 
     def __str__(self):
         board = ""
@@ -35,3 +41,26 @@ class World:
 
     def ready_player(self, session):
         print session.session_id
+        if session.session_id not in self.players:
+            new_player = player.Player()
+            self.players[session.session_id] = new_player
+            while True:
+                x = random.randint(0, World.size)
+                y = random.randint(0, World.size)
+
+                flag = False
+
+                for obj in self.cells[x][y].objects:
+                    if isinstance(obj, player.Player):
+                        flag = True
+
+                if flag == False:
+                    continue
+
+                self.cells[x][y].objects.append(new_player)
+                new_player.container = self.cells[x][y]
+
+                break
+        else:
+            # we are already ready
+            pass
