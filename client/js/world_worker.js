54ca665ab32d63a1ff6a00b3c7fcfc5ad898baaf
Alex Laties <alex.laties@gmail.com>
2011-12-25 03:43:23 -0500

    :/

--- client/js/world_worker.js
+++ client/js/world_worker.js
@@ -1,19 +1,16 @@
 var xhr;
-
-function onmessage(event) {
-  xhr = new XMLHttpRequest();
-  xhr.open('GET', "http://www.aperaturetesting.com:8080/ajax/snapshot");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState != 4)
+xhr = new XMLHttpRequest();
+xhr.open('GET', "http://www.aperaturetesting.com:8080/ajax/snapshot");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState != 4)
+  {
+    if (xhr.status != 200)
     {
-      if (xhr.status != 200)
-      {
-        alert("oh god i'm on fire!")
-      }
-      return
+      alert("oh god i'm on fire!")
     }
-    
-    postMessage({world: xhr.responseText, worldID: 1});
-  };
-  xhr.send();
-}
+    return
+  }
+  
+  postMessage({world: xhr.responseText, worldID: 1});
+};
+xhr.send();
