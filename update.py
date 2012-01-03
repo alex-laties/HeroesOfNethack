173af109add3dba9f1e022ede01c6bce0e71d91c
Alex Laties <alex.laties@gmail.com>
2012-01-02 23:04:55 -0500

    Added the ability to update the client to a specific branch

--- update.py
+++ update.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 from subprocess import call
 import sys
 import getopt
 from random import choice
 import string
+from os import chdir
 
 repo = "git://github.com/alex-laties/HeroesOfNethack.git"
 
@@ -11,8 +12,9 @@ def main():
   opts = 0
   args = 0
   target = ''
+  branch = 'master'
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "t:", ["target="])
+    opts, args = getopt.getopt(sys.argv[1:], "t: b:", ["target=", "branch="])
   except getopt.error, msg:
     print msg
     sys.exit(2)
@@ -20,6 +22,8 @@ def main():
   for o, a in opts:
    if o in ("-t", "--target"):
     target = a
+   elif o in ("-b, --branch"):
+    branch = a
 
   if target == '':
     print "no target"
@@ -35,6 +39,9 @@ def main():
   call(["mkdir", temp])
   print "cloning..."
   call(["git", "clone", repo, temp])
+  print "selecting branch"
+  chdir(temp)
+  call(["git", "checkout", branch])
   print "copying..."  
   call(["cp", "-r" , temp + "/client/index.html", temp+"/client/css", temp+"/client/js", target])
   print "removing temp..."
